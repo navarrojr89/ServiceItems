9c19d196e307f50d5b15d0c7e72854580a1fd905
Luis Navarro <navarrojr89@gmail.com>
2021-02-14 12:09:18 +0100

    Includes comments in the test class

--- src/test/java/com/lamn/microservices/serviceitems/ServiceItemsApplicationTests.java
+++ src/test/java/com/lamn/microservices/serviceitems/ServiceItemsApplicationTests.java
@@ -3,9 +3,15 @@ package com.lamn.microservices.serviceitems;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * The type Service items application tests.
+ */
 @SpringBootTest
 class ServiceItemsApplicationTests {
 
+	/**
+	 * Context loads.
+	 */
 	@Test
 	void contextLoads() {
 	}
