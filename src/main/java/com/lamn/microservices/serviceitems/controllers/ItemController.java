d65a1a401abb0fe32a65f9386fc8380404311d67
Luis Navarro <navarrojr89@gmail.com>
2021-02-16 16:16:23 +0100

    Removes the configuration from local

--- src/main/java/com/lamn/microservices/serviceitems/controllers/ItemController.java
+++ src/main/java/com/lamn/microservices/serviceitems/controllers/ItemController.java
@@ -4,28 +4,19 @@ import com.lamn.microservices.serviceitems.models.Item;
 import com.lamn.microservices.serviceitems.models.Product;
 import com.lamn.microservices.serviceitems.models.service.ItemService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * The type Item controller.
  */
 @RestController
 public class ItemController {
-
-    private static final Logger LOG = LoggerFactory.getLogger(ItemController.class);
 
     @Autowired
     @Qualifier("itemServiceFeign")
@@ -66,21 +57,4 @@ public class ItemController {
 
         return item;
     }
-
-    /**
-     * Obtain config response entity.
-     *
-     * @param configText the config text
-     * @param serverPort the server port
-     * @return the response entity
-     */
-    @GetMapping("/obtain-config")
-    public ResponseEntity<?> obtainConfig(@Value("${configuration.text}") String configText, @Value("${server.port}") String serverPort) {
-        LOG.info("Text [{}] and Port [{}]", configText, serverPort);
-        Map<String, String> jsonMap = new HashMap<>();
-        jsonMap.put("configText", configText);
-        jsonMap.put("serverPort", serverPort);
-        return new ResponseEntity<>(jsonMap, HttpStatus.OK);
-    }
-
 }
